85ac8810f4db5019b007d1263cbd06f80a0508c1
Cyteee <coyotemalandro@gmail.com>
2026-02-14 21:32:11 -0300

    Union, Intersection, difference.

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,17 +8,20 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Set<String> set = new LinkedHashSet<>();
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 
-        set.add("Monitor");
-        set.add("Iphone");
-        set.add("Desktop");
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
 
-        System.out.println(set.contains("Iphone"));
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println(d);
 
-        for (String p : set){
-            System.out.println(p);
-        }
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println(e);
 
         sc.close();
     }
